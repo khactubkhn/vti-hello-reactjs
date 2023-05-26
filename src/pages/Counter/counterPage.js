0d425f94c5f805b8b10d48ffc75a350f32de8c77
fourmfv <nguyen.khac.tu@moneyforward.vn>
2023-05-26 14:55:37 +0700

    react redux

--- src/pages/Counter/counterPage.js
+++ src/pages/Counter/counterPage.js
@@ -1,35 +1,45 @@
 import {useState} from 'react';
-import store from '../../store/store';
+import {connect} from 'react-redux';
 
-const CounterPage = () => {
-  const [counter, setCounter] = useState(0);
+const CounterPage = ({counter, increment, decrement}) => {
 
   const handleIncreaseCounter = () => {
-    store.dispatch({
-      type: 'counter/increment'
-    });
+    increment();
   };
 
   const handleDecreaseCounter = () => {
-    store.dispatch({
-      type: 'counter/decrement'
-    });
+   decrement();
   };
-
-  const updateData = () => {
-    store.subscribe(() => setCounter(store.getState().counter))
-  }
-
-  updateData();
 
   return (
       <>
         <h2>Counter page</h2>
-        <h2>{store.getState().counter}</h2>
+        <h2>{counter}</h2>
         <button onClick={handleIncreaseCounter}>Increase</button>
         <button onClick={handleDecreaseCounter}>Decrease</button>
       </>
   );
 };
 
-export default CounterPage;
+const mapGlobalStateToProps = state => {
+  return {
+    counter: state.counter
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    increment: () => {
+      dispatch({
+        type: 'counter/increment'
+      });
+    },
+    decrement: () => {
+      dispatch({
+        type: 'counter/decrement'
+      });
+    }
+  }
+}
+
+export default connect(mapGlobalStateToProps, mapDispatchToProps)(CounterPage);
